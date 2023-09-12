740f3e8f0c5b18be6c3365494f848629d6950876
Cross <joselidue@gmail.com>
2023-09-12 22:27:24 +0200

    Create Write_Backwards.py

---
+++ Write_Backwards.py
@@ -0,0 +1,17 @@
+word= input('Your word:')
+quant=len(word)
+index=quant
+while index > 0:
+    letter= word[index-1]
+    print(letter)
+    index= index-1
+    continue
+    print('Try again')
+    
+
+    
+    
+
+
+
+   
